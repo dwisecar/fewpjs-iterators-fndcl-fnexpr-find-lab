b629a88e375f34295918b1217919383eb0392580
dwisecar <dwisecar@gmail.com>
2021-01-16 10:53:19 -0800

    done

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr){
+  const season = arr.find(record => {return record.result == "W"});
+  if (season) {return season.year;}
+}
